626909c21c97bb0bfb62d991c83c363ace342fd1
Christophe Levis <christophe.levis@sonarsource.com>
2020-08-12 12:05:18 +0200

    Add issues

--- backend/src/main/java/foo/Foo.java
+++ backend/src/main/java/foo/Foo.java
@@ -6,10 +6,22 @@ package foo;
 public class Foo {
 
     public static int div(int a, int b) {
+	@Nonnull
+	private String primary;
+
+	primary = null;
+
+	int j;
+	while (true) { // Noncompliant; end condition omitted
+	  j++;
+	}
+
     	if (b == 0) {
     		throw new UnsupportedOperationException("Can't divide by zero!");
     	}
         return a / b;  
     }
 
+    public static int test(int a, int b) {
+    }
 }
--- backend/src/test/java/foo/FooTest.java
+++ backend/src/test/java/foo/FooTest.java
@@ -5,8 +5,14 @@ import static org.junit.Assert.*;
 
 public class FooTest {
 
-  @Test
+  //@Test
   public void testAdd() throws Exception {
   	assertEquals(Foo.div(10, 5), 2);
+
+	System.runFinalizersOnExit(true);
+	throw new RuntimeException("My Message");
+  }
+
+  public void testVoid() throws Exception {
   }
 }
